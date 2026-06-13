b31947e48b72c978c6296db880e87020439026d4
LucidPolo12 <mbartoloni12@gmail.com>
2026-06-13 16:55:28 -0500

    Add Boompa bobblehead

--- regenerate_data.py
+++ regenerate_data.py
@@ -17,7 +17,8 @@ PCOLS = [("Ewan","I","J"),("Nana","L","M"),("Emrys","O","P"),("Nonno","R","S"),
          ("Autumn","AD","AE"),("River","AG","AH")]
 CHAMPS = {"Ewan":"Spain","Nana":"France","Nonno":"Brazil","Boompa":"France"}
 PHOTOS = {"Nonno":"nonno-bobblehead.jpg","Emrys":"emrys-bobblehead.jpg",
-          "Nana":"nana-bobblehead.jpg","Nonna":"nonna-bobblehead.jpg"}
+          "Nana":"nana-bobblehead.jpg","Nonna":"nonna-bobblehead.jpg",
+          "Boompa":"boompa-bobblehead.jpg"}
 
 def pts(ph, pa, ah, aa):
     if None in (ph, pa, ah, aa): return None
